7f7fbe95ea3181b1c23d7c23c583c9d5af80fac8
Yurii <Yurii_Sokolenko@epam.com>
2023-10-27 13:31:38 +0300

    Unused CompositionLocalProviders removed.

--- app/src/main/java/com/xm/surveyapp/ui/screens/UI.kt
+++ app/src/main/java/com/xm/surveyapp/ui/screens/UI.kt
@@ -4,29 +4,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import com.xm.surveyapp.ui.navigation.AppNavController
-import com.xm.surveyapp.services.LocalApiRepository
-import com.xm.surveyapp.services.LocalQueriesRepository
-import com.xm.surveyapp.services.rememberApi
-import com.xm.surveyapp.services.rememberQueriesRepository
 import com.xm.surveyapp.ui.theme.SurveyAppTheme
 
 @Composable
 fun UI() {
     SurveyAppTheme {
-        CompositionLocalProvider(
-            LocalApiRepository provides rememberApi(),
-            LocalQueriesRepository provides rememberQueriesRepository()
-        ) {
-            Surface(
+        Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
                 AppNavController()
             }
-        }
     }
 
 }
